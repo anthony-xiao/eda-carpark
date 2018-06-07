4f8fa67bf486e99db5de6834e6896ef372fb5c31
Michael Mitchelson <michael.j.mitchelson@gmail.com>
2018-06-08 08:18:46 +1200

    fix values for park and leave

--- functions/lib.js
+++ functions/lib.js
@@ -1,13 +1,10 @@
 
-
-
-
 function leave (carparks, id) {
-  if (id > 6 <= 9) { return blocker(carparks, id, 'clear', 'green') } else { return free(carparks, id, 'clear') }
+  if (id > 6 || id <= 9) { return blocker(carparks, id, 'clear', 'green') } else { return free(carparks, id, 'clear') }
 }
 
 function park (carparks, id) {
-  if (id > 6 <= 9) { return blocker(carparks, id, 'green', 'red') } else { return free(carparks, id, 'green') }
+  if (id > 6 || id <= 9) { return blocker(carparks, id, 'green', 'red') } else { return free(carparks, id, 'green') }
 }
 
 function free (carparks, id, self) {
@@ -17,7 +14,7 @@ function free (carparks, id, self) {
 function blocker (carparks, id, self, other) {
   carparks.carparks[id - 1].colour = self
   carparks.carparks[id - 7].colour = other
-  if (id === 9) { carparks.carparks[10].colour = other }
+  if (id === 9) { carparks.carparks[9].colour = other }
   return carparks
 }
 
