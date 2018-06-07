fa2802af05b5fc4c09e97c9db3ec50af964b0e80
sarah-mclaughlin <sarahmclaughlin0@gmail.com>
2018-06-07 19:59:06 +1200

    add post function

--- routes/rootRoute.js
+++ routes/rootRoute.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const dataProc = require('../dataProc.js')
-//const changeData = require('../dataProc.js')
+// const changeData = require('../dataProc.js')
 
 router.get('/', (req, res) => {
   res.send('I am here!')
@@ -36,6 +36,33 @@ router.get('/carpark_test/:id', (req, res) => {
       const carData = allData.carparks.find(pup => pup.id === id)
       console.log(carData)
       res.render('./home/index', carData)
+    }
+  })
+})
+
+// change one car information by post method//////////////
+router.post('/carpark_test/edit/:id', (req, res) => {
+  const id = Number(req.params.id)
+  dataProc.getData((err, data) => {
+    if (err) {
+      res.send('unable to read data file').status(500)
+    } else {
+      const allData = JSON.parse(data)
+      const carData = allData.puppies.find(pup => pup.id === id)
+      carData.name = req.body.name
+      carData.rego = req.body.rego
+      carData.mobile = req.body.mobile
+      carData.status = req.body.status
+      carData.blocked = req.body.blocked
+      console.log(carData)
+      const newData = JSON.stringify(allData, null, 2)
+      // dataProc.changeData(newData, (err) => {
+      //  if (err) {
+      //    res.send('unable to save the file').status(500)
+      //  } else {
+      //    res.redirect('/puppies/' + id)
+      //  }
+      // })
     }
   })
 })
