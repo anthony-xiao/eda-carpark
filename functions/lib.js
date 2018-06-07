7078f8c63ab3738685ec292f1dfca9088a495f50
Michael Mitchelson <michael.j.mitchelson@gmail.com>
2018-06-07 20:57:41 +1200

    include export

--- functions/lib.js
+++ functions/lib.js
@@ -1,35 +1,13 @@
 
-// function park (carparks, id) {
-//   if (id < 4) { return student(carparks, id) }
-//   else if (id >= 4 < 10) { return 'green' }
-//   else { return corner(carparks, id) }
-// }
 
-// function student (carparks, id) {
-//   var blockercar = carparks.carparks[id + 5]
-//   if (blockercar.status) {
-//     return 'red'
-//   } else { return 'green' }
-// }
 
-// function corner (carparks, id) {
-//   var blockercar = carparks.carparks[id - 2]
-//   if (blockercar.status) {
-//     return 'red'
-//   } else { return 'green' }
-// }
 
-// function unParked (carparks, id) {
-//   for
-// }
 function leave (carparks, id) {
-  if (id > 6 <= 9) { return blocker(carparks, id, 'clear', 'green') }
-  else { return free(carparks, id, 'clear') }
+  if (id > 6 <= 9) { return blocker(carparks, id, 'clear', 'green') } else { return free(carparks, id, 'clear') }
 }
 
 function park (carparks, id) {
-  if (id > 6 <= 9) { return blocker(carparks, id, 'green', 'red') }
-  else { return free(carparks, id, 'green') }
+  if (id > 6 <= 9) { return blocker(carparks, id, 'green', 'red') } else { return free(carparks, id, 'green') }
 }
 
 function free (carparks, id, self) {
@@ -43,3 +21,7 @@ function blocker (carparks, id, self, other) {
   return carparks
 }
 
+module.exports = {
+  leave,
+  park
+}
