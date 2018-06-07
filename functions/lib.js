a146ee70a9626e4d9dcb9c0c43596328184b320d
Michael Mitchelson <michael.j.mitchelson@gmail.com>
2018-06-08 08:30:43 +1200

    fixing the parking and leaving functions

--- functions/lib.js
+++ functions/lib.js
@@ -3,12 +3,10 @@ const blocker = require('./blocker')
 
 
 function leave (carparks, id) {
-  if (id > 6 <= 9) { return blocker(carparks, id, 'clear', 'green') } else { return free(carparks, id, 'clear') }
+  if (id > 6 || id <= 9) { return blocker(carparks, id, 'clear', 'green') } else { return free(carparks, id, 'clear') }
 }
 
-function park (carparks, id) {
-  if (id > 6 <= 9) { return blocker(carparks, id, 'green', 'red') } else { return free(carparks, id, 'green') }
-}
+
 
 module.exports = {
   leave,
--- functions/park.js
+++ functions/park.js
@@ -2,5 +2,5 @@ const free = require('./free')
 const blocker = require('./blocker')
 
 module.exports = (carparks, id) => {
-  if (id > 6 <= 9) { return blocker(carparks, id, 'green', 'red') } else { return free(carparks, id, 'green') }
-}
+    if (id > 6 || id <= 9) { return blocker(carparks, id, 'green', 'red') } else { return free(carparks, id, 'green') }
+  }
