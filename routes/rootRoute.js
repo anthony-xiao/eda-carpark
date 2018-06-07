017aa640fd0389d95303360a295f3f00289b7425
sarah-mclaughlin <sarahmclaughlin0@gmail.com>
2018-06-08 11:48:15 +1200

    man so many changes to see how git works ...

--- routes/rootRoute.js
+++ routes/rootRoute.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const dataProc = require('../dataProc.js')
 const lib = require('../functions/lib')
+
 // const changeData = require('../dataProc.js')
 
 // router.get('/', (req, res) => {
@@ -20,7 +21,6 @@ router.get('/carpark', (req, res) => {
       res.send('unable to read data file').status(500)
     } else {
       const carParkData = JSON.parse(data)
-      console.log(carParkData)
       res.render('/main', carParkData)
       // res.send('Test success! [carpark_allCars_test] : ' + JSON.stringify(carParkData))
     }
@@ -36,7 +36,6 @@ router.get('/carpark/edit/:id', (req, res) => {
     } else {
       const allData = JSON.parse(data)
       const carData = allData.carparks.find(pup => pup.id === id)
-      console.log(carData)
       // res.render('edit', carData)
       res.send('Test success! [carpark_oneCar_test by id] : ' + JSON.stringify(carData))
     }
@@ -52,7 +51,6 @@ router.get('/carpark/view/:id', (req, res) => {
     } else {
       const allData = JSON.parse(data)
       const carData = allData.carparks.find(pup => pup.id === id)
-      console.log(carData)
       // res.render('view', carData)
       res.send('Test success! [carpark_oneCar_test by id] : ' + JSON.stringify(carData))
     }
@@ -72,8 +70,7 @@ router.post('/carpark/edit/:id', (req, res) => {
       carData.rego = req.body.rego
       carData.mobile = req.body.mobile
       carData.status = req.body.status
-      let updatedData = lib.park(allData)
-      console.log(carData)
+      let updatedData = lib.park(allData, id)
       const newData = JSON.stringify(updatedData, null, 2)
       dataProc.changeData(newData, (err) => {
         if (err) {
@@ -93,10 +90,9 @@ router.post('/carpark/view/:id', (req, res) => {
       res.send('unable to read data file').status(500)
     } else {
       const allData = JSON.parse(data)
-      let carPark = allData.carparks.find(pup => pup.id === id)
-      carPark = lib.clear(carPark)
-      carPark = lib.leave(allData, id)
-      const newData = JSON.stringify(allData, null, 2)
+      allData.carparks[id - 1] = lib.clear(allData.carparks.find(pup => pup.id === id))
+      let updatedData = lib.leave(allData, id)
+      const newData = JSON.stringify(updatedData, null, 2)
       dataProc.changeData(newData, (err) => {
         if (err) {
           res.send('unable to save the file').status(500)
