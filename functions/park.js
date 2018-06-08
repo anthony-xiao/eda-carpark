9ed2ba759ef7bc6286b527409b90603613060889
anthony-xiao <juniorxiao01@gmail.com>
2018-06-08 15:27:02 +1200

    added in change url

--- functions/park.js
+++ functions/park.js
@@ -2,6 +2,7 @@ const free = require('./free')
 const blocker = require('./blocker')
 
 module.exports = (carparks, id) => {
+  carparks.carparks[id - 1].url = '/carpark/edit/' + id
   if (id > 6 && id <= 9) {
     return blocker(carparks, id, 'green', 'red')
   } else {
